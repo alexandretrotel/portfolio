0c3129b46fc4c1b90244f6dd1f03d3ee6ae62cda
Alexandre Trotel <trotelalexandre@proton.me>
2024-12-04 12:39:07 +0100

    fix: page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default async function Blog() {
-  return <></>;
+  return (
+    <p className="text-muted-foreground text-sm">
+      I&apos;m so excited to share my thoughts with you but you&apos;ll have to
+      wait a little longer, just the time that it takes to write a book I think.
+      In the meantime, you can check out my{" "}
+      <Link href="/contact" className="text-primary hover:underline">
+        contact
+      </Link>{" "}
+      page.
+    </p>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <>ttt</>;
+  return (
+    <p className="text-muted-foreground text-sm">
+      I know I&apos;m pretty fast but this is still under construction. Please
+      come back maybe yesterday. In the meantime, you can check out my{" "}
+      <Link href="/contact" className="text-primary hover:underline">
+        contact
+      </Link>{" "}
+      page.
+    </p>
+  );
 }
