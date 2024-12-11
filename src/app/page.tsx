0c3eb660dcd6629642e98037b8136632fc3cf5e5
Alexandre Trotel <113524174+trotelalexandre@users.noreply.github.com>
2024-12-11 19:00:56 +0100

    change: text (#1)
    * fix: clean dependencies
    
    * fix: text
    
    * fix: react version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <p className="text-muted-foreground text-sm">
-      I know I&apos;m pretty fast but this is still under construction. Please
-      come back maybe yesterday. In the meantime, you can check out my{" "}
+      This page isn&apos;t ready yet but you can check out the{" "}
       <Link href="/contact" className="text-primary hover:underline">
         contact
       </Link>{" "}
