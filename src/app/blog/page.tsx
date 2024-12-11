087ecda43ea100c5acb19171d2d04a12c389180a
Alexandre Trotel <113524174+trotelalexandre@users.noreply.github.com>
2024-12-11 19:00:56 +0100

    change: text (#1)
    * fix: clean dependencies
    
    * fix: text
    
    * fix: react version

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,9 +3,7 @@ import Link from "next/link";
 export default async function Blog() {
   return (
     <p className="text-muted-foreground text-sm">
-      I&apos;m so excited to share my thoughts with you but you&apos;ll have to
-      wait a little longer, just the time that it takes to write a book I think.
-      In the meantime, you can check out my{" "}
+      This page isn&apos;t ready yet but you can check out the{" "}
       <Link href="/contact" className="text-primary hover:underline">
         contact
       </Link>{" "}
