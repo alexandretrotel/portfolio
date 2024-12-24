f96110711b683d9dd0d641af471dece1ed117fd5
Alexandre Trotel <trotelalexandre@proton.me>
2024-12-24 09:38:16 +0100

    fix: header

--- src/components/core/header.tsx
+++ src/components/core/header.tsx
@@ -8,7 +8,13 @@ import Link from "next/link";
 import { DarkModeToggle } from "@/components/ui/dark-mode-toggle";
 import Image from "next/image";
 
-const components: { title: string; href: string }[] = [
+interface ComponentProps {
+  title: string;
+  href: string;
+  disabled?: boolean;
+}
+
+const components: ComponentProps[] = [
   {
     title: "Home",
     href: "/",
@@ -16,6 +22,15 @@ const components: { title: string; href: string }[] = [
   {
     title: "Blog",
     href: "/blog",
+  },
+  {
+    title: "Projects",
+    href: "/projects",
+    disabled: true,
+  },
+  {
+    title: "Games",
+    href: "/games",
   },
   {
     title: "Contact",
@@ -39,15 +54,21 @@ export default async function Header() {
           </Link>
           <NavigationMenu>
             <NavigationMenuList className="flex items-center md:gap-6">
-              {components.map((component) => (
-                <NavigationMenuItem key={component.title}>
-                  <Link href={component.href} legacyBehavior passHref>
-                    <NavigationMenuLink className="group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 bg-transparent">
-                      {component.title}
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-              ))}
+              {components.map((component) => {
+                if (component.disabled) {
+                  return null;
+                }
+
+                return (
+                  <NavigationMenuItem key={component.title}>
+                    <Link href={component.href} legacyBehavior passHref>
+                      <NavigationMenuLink className="group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 bg-transparent">
+                        {component.title}
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                );
+              })}
             </NavigationMenuList>
           </NavigationMenu>
           <DarkModeToggle />
