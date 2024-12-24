daf1546e2c5c9e7e5e8581a2cd0876486722434e
Alexandre Trotel <trotelalexandre@proton.me>
2024-12-24 10:43:42 +0100

    fix: hook

--- src/app/(core)/games/quatio/page.tsx
+++ src/app/(core)/games/quatio/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 import "client-only";
 
-import { generateEquation } from "@/utils/quatio/generateEquation";
-import { useCallback, useEffect, useState } from "react";
 import { Difficulty } from "@/types/quatio";
 import {
   Card,
@@ -21,61 +19,22 @@ import {
 } from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import useQuatio from "@/hooks/games/quatio/useQuatio";
 
 export default function Quatio() {
-  const [difficulty, setDifficulty] = useState<Difficulty>("easy");
-  const [unknowns, setUnknowns] = useState(1);
-  const [equationData, setEquationData] = useState(generateEquation("easy", 1));
-  const [userAnswer, setUserAnswer] = useState("");
-  const [feedback, setFeedback] = useState("");
-  const [isCorrect, setIsCorrect] = useState(false);
-
-  const newEquation = useCallback(() => {
-    setEquationData(generateEquation(difficulty, unknowns));
-    setUserAnswer("");
-    setFeedback("");
-    setIsCorrect(false);
-  }, [difficulty, unknowns]);
-
-  const checkAnswer = useCallback(() => {
-    const { solution } = equationData;
-
-    if (unknowns === 1) {
-      const correct = parseFloat(userAnswer) === parseFloat(solution as string);
-      setIsCorrect(correct);
-      setFeedback(
-        correct ? "Correct!" : `Incorrect! The correct answer is ${solution}`
-      );
-    } else {
-      const [x, y] = userAnswer.split(",").map((val) => parseFloat(val.trim()));
-      const solutionObj = solution as { x: string; y: string };
-      const correct =
-        x === parseFloat(solutionObj.x) && y === parseFloat(solutionObj.y);
-      setIsCorrect(correct);
-      setFeedback(
-        correct
-          ? "Correct!"
-          : `Incorrect! The correct answer is (${solutionObj.x}, ${solutionObj.y})`
-      );
-    }
-
-    setTimeout(() => {
-      newEquation();
-    }, 2000);
-  }, [equationData, unknowns, userAnswer, newEquation]);
-
-  useEffect(() => {
-    newEquation();
-  }, [difficulty, unknowns, newEquation]);
-
-  const handleKeyPress = useCallback(
-    (event: React.KeyboardEvent<HTMLInputElement>) => {
-      if (event.key === "Enter") {
-        checkAnswer();
-      }
-    },
-    [checkAnswer]
-  );
+  const {
+    difficulty,
+    setDifficulty,
+    unknowns,
+    setUnknowns,
+    equationData,
+    userAnswer,
+    setUserAnswer,
+    feedback,
+    isCorrect,
+    checkAnswer,
+    handleKeyPress,
+  } = useQuatio();
 
   return (
     <div className="flex flex-col items-center justify-center h-full">
