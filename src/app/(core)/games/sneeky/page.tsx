dff0dcdb2043c9e308072839b147628d2dfc1bd9
Alexandre Trotel <trotelalexandre@proton.me>
2024-12-24 15:14:36 +0100

    add: images

---
+++ src/app/(core)/games/sneeky/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+import Sneeky from "./sneeky";
+
+export const metadata: Metadata = {
+  title: "Sneeky",
+  description: "Sneeky is a remaster of the classic Snake game.",
+  openGraph: {
+    title: "Sneeky",
+    description: "Sneeky is a remaster of the classic Snake game.",
+  },
+};
+
+export default async function Page() {
+  return <Sneeky />;
+}
