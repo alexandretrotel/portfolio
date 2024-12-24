b87b85d247427e27c72daf7cedc35a0fbcc3cbbf
Alexandre Trotel <trotelalexandre@proton.me>
2024-12-24 15:14:36 +0100

    add: images

--- src/app/(core)/games/page.tsx
+++ src/app/(core)/games/page.tsx
@@ -6,27 +6,51 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Image from "next/image";
 import Link from "next/link";
+
+const games = [
+  {
+    name: "Quatio",
+    description:
+      "Quatio is a game where you have to solve equations to get the highest score.",
+    href: "/games/quatio",
+    image: "/games/quatio.png",
+  },
+  {
+    name: "Sneeky",
+    description: "Sneeky is a remaster of the classic Snake game.",
+    href: "/games/sneeky",
+    image: "/games/sneeky.png",
+  },
+];
 
 export default async function Games() {
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-2xl font-bold">Games</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mx-auto max-w-5xl">
-        <Card>
-          <CardHeader>
-            <CardTitle>Quatio, the math game</CardTitle>
-            <CardDescription>
-              Quatio is a game where you have to solve equations to get the
-              highest score.
-            </CardDescription>
-          </CardHeader>
-          <CardFooter>
-            <Button asChild>
-              <Link href="/games/quatio">Play</Link>
-            </Button>
-          </CardFooter>
-        </Card>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mx-auto max-w-5xl">
+        {games.map((game) => (
+          <Card key={game.name} className="flex flex-col justify-between">
+            <CardHeader>
+              <div className="flex items-center justify-between">
+                <CardTitle>{game.name}</CardTitle>
+                <Image
+                  src={game.image}
+                  alt={game.name}
+                  width={32}
+                  height={32}
+                />
+              </div>
+              <CardDescription>{game.description}</CardDescription>
+            </CardHeader>
+            <CardFooter>
+              <Button asChild>
+                <Link href={game.href}>Play now</Link>
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
       </div>
     </div>
   );
