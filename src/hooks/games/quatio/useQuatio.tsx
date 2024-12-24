51584a8a09d24f44ef1de8d166981521068d4fee
Alexandre Trotel <trotelalexandre@proton.me>
2024-12-24 10:43:42 +0100

    fix: hook

---
+++ src/hooks/games/quatio/useQuatio.tsx
@@ -0,0 +1,73 @@
+import { Difficulty } from "@/types/quatio";
+import { generateEquation } from "@/utils/quatio/generateEquation";
+import { useCallback, useEffect, useState } from "react";
+
+export default function useQuatio() {
+  const [difficulty, setDifficulty] = useState<Difficulty>("easy");
+  const [unknowns, setUnknowns] = useState(1);
+  const [equationData, setEquationData] = useState(generateEquation("easy", 1));
+  const [userAnswer, setUserAnswer] = useState("");
+  const [feedback, setFeedback] = useState("");
+  const [isCorrect, setIsCorrect] = useState(false);
+
+  const newEquation = useCallback(() => {
+    setEquationData(generateEquation(difficulty, unknowns));
+    setUserAnswer("");
+    setFeedback("");
+    setIsCorrect(false);
+  }, [difficulty, unknowns]);
+
+  const checkAnswer = useCallback(() => {
+    const { solution } = equationData;
+
+    if (unknowns === 1) {
+      const correct = parseFloat(userAnswer) === parseFloat(solution as string);
+      setIsCorrect(correct);
+      setFeedback(
+        correct ? "Correct!" : `Incorrect! The correct answer is ${solution}`
+      );
+    } else {
+      const [x, y] = userAnswer.split(",").map((val) => parseFloat(val.trim()));
+      const solutionObj = solution as { x: string; y: string };
+      const correct =
+        x === parseFloat(solutionObj.x) && y === parseFloat(solutionObj.y);
+      setIsCorrect(correct);
+      setFeedback(
+        correct
+          ? "Correct!"
+          : `Incorrect! The correct answer is (${solutionObj.x}, ${solutionObj.y})`
+      );
+    }
+
+    setTimeout(() => {
+      newEquation();
+    }, 2000);
+  }, [equationData, unknowns, userAnswer, newEquation]);
+
+  useEffect(() => {
+    newEquation();
+  }, [difficulty, unknowns, newEquation]);
+
+  const handleKeyPress = useCallback(
+    (event: React.KeyboardEvent<HTMLInputElement>) => {
+      if (event.key === "Enter") {
+        checkAnswer();
+      }
+    },
+    [checkAnswer]
+  );
+
+  return {
+    difficulty,
+    setDifficulty,
+    unknowns,
+    setUnknowns,
+    equationData,
+    userAnswer,
+    setUserAnswer,
+    feedback,
+    isCorrect,
+    checkAnswer,
+    handleKeyPress,
+  };
+}
