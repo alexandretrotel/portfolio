dacdcab89187bb8cc8d60d595044e66d40d14f1e
Alexandre Trotel <trotelalexandre@proton.me>
2024-12-30 13:41:11 +0100

    fix: skills

--- src/app/(core)/skills.tsx
+++ src/app/(core)/skills.tsx
@@ -4,20 +4,16 @@ import "client-only";
 import { Badge } from "@/components/ui/badge";
 import { skills } from "@/data/skills";
 import { ArrowUpRight } from "lucide-react";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
 import { motion } from "motion/react";
 import Link from "next/link";
+import { useMemo, useState } from "react";
+import { Category, Skills } from "@/types/skills";
 
 interface SkillsProps {
   delay: number;
 }
 
-export default function Skills({ delay }: SkillsProps) {
+export default function SkillsSection({ delay }: SkillsProps) {
   return (
     <motion.div
       className="flex flex-col"
@@ -27,6 +23,26 @@ export default function Skills({ delay }: SkillsProps) {
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5, delay }}
     >
+      <StaticSkillsContent />
+    </motion.div>
+  );
+}
+
+function StaticSkillsContent() {
+  const [selectedGroup, setSelectedGroup] = useState<Category | null>(null);
+
+  const filteredSkills: Skills = useMemo(() => {
+    if (selectedGroup === null) {
+      return skills.flatMap((group) => group.skills);
+    }
+
+    return skills
+      .filter((group) => group.category === selectedGroup)
+      .flatMap((group) => group.skills);
+  }, [selectedGroup]);
+
+  return (
+    <div>
       <h1 className="text-lg font-semibold">Skills</h1>
       <div className="flex flex-col gap-4">
         <p className="text-sm text-muted-foreground">
@@ -34,36 +50,51 @@ export default function Skills({ delay }: SkillsProps) {
           some of the skills I&apos;ve picked up along the way in a
           non-exhaustive list.
         </p>
-        <Accordion type="single" collapsible className="space-y-4">
+
+        <div className="flex flex-wrap gap-2">
+          <Badge
+            className="cursor-pointer"
+            variant={selectedGroup === null ? "default" : "outline"}
+            onClick={() => setSelectedGroup(null)}
+          >
+            All
+          </Badge>
+
           {skills
             ?.sort((a, b) => a.category.localeCompare(b.category))
             ?.map((group, groupIndex) => (
-              <AccordionItem key={groupIndex} value={group.category}>
-                <AccordionTrigger className="text-lg font-semibold py-2">
-                  {group.category}
-                </AccordionTrigger>
-                <AccordionContent className="pb-2">
-                  <div className="flex flex-wrap gap-2">
-                    {group?.skills
-                      ?.sort((a, b) => a.skill.localeCompare(b.skill))
-                      ?.map((item, itemIndex) =>
-                        item?.url ? (
-                          <Link key={itemIndex} href={item.url} target="_blank">
-                            <Badge className="cursor-pointer flex items-center">
-                              {item.skill}
-                              <ArrowUpRight size={16} className="ml-1" />
-                            </Badge>
-                          </Link>
-                        ) : (
-                          <Badge key={itemIndex}>{item.skill}</Badge>
-                        )
-                      )}
-                  </div>
-                </AccordionContent>
-              </AccordionItem>
+              <Badge
+                key={groupIndex}
+                className="cursor-pointer"
+                variant={
+                  selectedGroup === group.category ? "default" : "outline"
+                }
+                onClick={() => setSelectedGroup(group.category)}
+              >
+                {group.category}
+              </Badge>
             ))}
-        </Accordion>
+        </div>
+
+        <div className="flex flex-wrap gap-2">
+          {filteredSkills
+            ?.sort((a, b) => a.skill.localeCompare(b.skill))
+            ?.map((item) => {
+              if (item?.url) {
+                return (
+                  <Link key={item.skill} href={item.url} target="_blank">
+                    <Badge>
+                      {item.skill}
+                      <ArrowUpRight size={16} className="ml-1" />
+                    </Badge>
+                  </Link>
+                );
+              } else {
+                return <Badge key={item.skill}>{item.skill}</Badge>;
+              }
+            })}
+        </div>
       </div>
-    </motion.div>
+    </div>
   );
 }
