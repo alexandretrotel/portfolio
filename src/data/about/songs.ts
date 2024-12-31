10ecc5579282b7675e07703bb196434f19c43897
Alexandre Trotel <trotelalexandre@proton.me>
2024-12-31 14:20:09 +0100

    fix: song item layout

--- src/data/about/songs.ts
+++ src/data/about/songs.ts
@@ -1,4 +1,4 @@
-import { Songs } from "@/types/about/songs";
+import { RecordLabelItem, Songs } from "@/types/about/songs";
 import { RiSpotifyFill, RiYoutubeFill } from "@remixicon/react";
 import React from "react";
 
@@ -7,6 +7,13 @@ const coverBasePath: string = `${songsBasePath}/cover`;
 const assetsBasePath: string = `${songsBasePath}/assets`;
 
 const ICON_SIZE: number = 16;
+
+export const recordLabels: { [key: string]: RecordLabelItem } = {
+  SelfReleased: {
+    name: "Self Released",
+    url: "https://www.alexandretrotel.org",
+  },
+};
 
 export const songs: Songs = [
   {
@@ -39,6 +46,68 @@ export const songs: Songs = [
     ],
     styles: ["Future House", "EDM"],
     date: new Date("2022-09-14"),
-    labels: [{ name: "Self Released", url: "https://www.alexandretrotel.org" }],
+    labels: [recordLabels["SelfReleased"]],
+  },
+  {
+    slug: "journey",
+    title: "Journey",
+    description:
+      "My first song under the name of Alexandre Trotel. Journey symbolizes the beginning of a new chapter in my life.",
+    authors: ["Alexandre Trotel"],
+    status: "Finished",
+    links: [
+      {
+        platform: "YouTube",
+        url: "https://www.youtube.com/watch?v=FUR1ea1ilUU",
+        icon: React.createElement(RiYoutubeFill, { size: ICON_SIZE }),
+      },
+      {
+        platform: "Spotify",
+        url: "https://open.spotify.com/track/4jEHcBr18knr95OLpcMvaK?si=b991b87d2e6b47d9",
+        icon: React.createElement(RiSpotifyFill, { size: ICON_SIZE }),
+      },
+    ],
+    cover: `${coverBasePath}/journey.jpg`,
+    assets: [
+      {
+        type: "Audio",
+        url: `${assetsBasePath}/journey.mp3`,
+        version: "Final",
+      },
+    ],
+    styles: ["Progressive House", "EDM"],
+    date: new Date("2022-10-05"),
+    labels: [recordLabels["SelfReleased"]],
+  },
+  {
+    slug: "angelic-soul",
+    title: "Angelic Soul",
+    description:
+      "A song that I made for my girlfriend, Angelic Soul is some kind of Trance and Happy Hardcore mix.",
+    authors: ["Alexandre Trotel"],
+    status: "Finished",
+    links: [
+      {
+        platform: "YouTube",
+        url: "https://www.youtube.com/watch?v=ig70lvXT9nI",
+        icon: React.createElement(RiYoutubeFill, { size: ICON_SIZE }),
+      },
+      {
+        platform: "Spotify",
+        url: "https://open.spotify.com/track/5UQ7A0ydTGJeMaGOVTJlmQ?si=cdecfe2d46c249de",
+        icon: React.createElement(RiSpotifyFill, { size: ICON_SIZE }),
+      },
+    ],
+    cover: `${coverBasePath}/angelic-soul.jpg`,
+    assets: [
+      {
+        type: "Audio",
+        url: `${assetsBasePath}/angelic-soul.mp3`,
+        version: "Final",
+      },
+    ],
+    styles: ["Trance", "Happy Hardcore"],
+    date: new Date("2022-08-02"),
+    labels: [recordLabels["SelfReleased"]],
   },
 ];
