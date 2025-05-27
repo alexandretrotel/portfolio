13857c3a39eb113ce3db2b2891741fb12fd29cc3
Alexandre Trotel <trotelalexandre@proton.me>
2025-05-27 22:04:06 +0200

    refactor: simplify project data handling and remove unused fields

--- src/app/(core)/projects/[slug]/project-header.tsx
+++ src/app/(core)/projects/[slug]/project-header.tsx
@@ -1,7 +1,6 @@
 import { Separator } from "@/components/ui/separator";
 import { features } from "@/data/features";
 import type { Project } from "@/types/projects/projects";
-import { CheckIcon, PauseIcon, XCircleIcon } from "lucide-react";
 
 export default function ProjectHeader({
   project,
@@ -10,13 +9,6 @@ export default function ProjectHeader({
   project: Project;
   count: number;
 }) {
-  const statusIcon = {
-    WIP: <PauseIcon className="inline-block h-5 w-5 text-yellow-500" />,
-    Completed: <CheckIcon className="inline-block h-5 w-5 text-green-500" />,
-    Paused: <PauseIcon className="inline-block h-5 w-5 text-blue-500" />,
-    Archived: <XCircleIcon className="inline-block h-5 w-5 text-red-500" />,
-  };
-
   const formattedCount = new Intl.NumberFormat("en-US").format(count);
 
   return (
@@ -33,30 +25,8 @@ export default function ProjectHeader({
 
       <div className="flex h-5 flex-wrap items-center gap-4 text-sm">
         {features.enableProjectStatus && (
-          <>
-            <span className="flex items-center gap-1">
-              {statusIcon[project.status]}
-              {project.status}
-            </span>
-
-            <Separator orientation="vertical" className="h-full" />
-          </>
+          <Separator orientation="vertical" className="h-full" />
         )}
-
-        <span className="flex items-center gap-1">
-          {new Date(project.date).toLocaleDateString("en-US", {
-            month: "long",
-            day: "numeric",
-            year: "numeric",
-          })}{" "}
-          {project.dateCompleted && " - "}
-          {project.dateCompleted &&
-            new Date(project.dateCompleted).toLocaleDateString("en-US", {
-              month: "long",
-              day: "numeric",
-              year: "numeric",
-            })}
-        </span>
       </div>
     </header>
   );
