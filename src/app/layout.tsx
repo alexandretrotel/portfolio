9fc5a52545be79f5cc80a56f212754987d44bcba
Alexandre Trotel <trotelalexandre@proton.me>
2025-06-19 20:18:55 +0200

    chore: update globals.css and fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,50 +1,54 @@
 import "./globals.css";
 
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/providers/theme-provider";
 
-const font = Inter({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
+const font = Geist({
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	subsets: ["latin"],
+});
+const fontMono = Geist_Mono({
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Alexandre Trotel | Official Website",
-  description:
-    "Alexandre Trotel is a tech entrepreneur and full-stack developer with expertise in blockchain, web development, and innovation.",
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: "https://www.alexandretrotel.org",
-    title: "Alexandre Trotel | Official Website",
-    description:
-      "Alexandre Trotel is a tech entrepreneur and full-stack developer with expertise in blockchain, web development, and innovation.",
-  },
-  twitter: {
-    title: "Alexandre Trotel | Official Website",
-    description:
-      "Alexandre Trotel is a tech entrepreneur and full-stack developer with expertise in blockchain, web development, and innovation.",
-  },
+	title: "Alexandre Trotel | Official Website",
+	description:
+		"Alexandre Trotel is a tech entrepreneur and full-stack developer with expertise in blockchain, web development, and innovation.",
+	openGraph: {
+		type: "website",
+		locale: "en_US",
+		url: "https://www.alexandretrotel.org",
+		title: "Alexandre Trotel | Official Website",
+		description:
+			"Alexandre Trotel is a tech entrepreneur and full-stack developer with expertise in blockchain, web development, and innovation.",
+	},
+	twitter: {
+		title: "Alexandre Trotel | Official Website",
+		description:
+			"Alexandre Trotel is a tech entrepreneur and full-stack developer with expertise in blockchain, web development, and innovation.",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${font.className} antialiased`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body className={`${font.className} ${fontMono.className} antialiased`}>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
