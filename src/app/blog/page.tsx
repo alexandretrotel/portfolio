f39bf6ea65e671ace93984e3d03b7ead65a23cce
Alexandre Trotel <trotelalexandre@proton.me>
2025-06-24 08:54:41 +0200

    refactor: improve layout and styling of blog post links for better user experience

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,39 +4,46 @@ import { cn } from "@/lib/utils";
 import { Animation } from "@/components/animation";
 
 export default async function Blog() {
-  const posts = await getBlogPosts();
+	const posts = await getBlogPosts();
 
-  return (
-    <Animation>
-      <div className="flex flex-col gap-6">
-        <ul className="flex flex-col">
-          {posts.map((post) => (
-            <li key={post.slug}>
-              <Link
-                href={`/blog/${post.slug}`}
-                aria-label={`Read blog post: ${post.title}`}
-                className="group hover:bg-muted/50 flex items-center gap-4 rounded-sm p-2 py-4 transition-colors"
-              >
-                <span
-                  className={cn("text-muted-foreground w-20 shrink-0 text-xs", {
-                    "opacity-0": !post.showDate,
-                  })}
-                >
-                  {post.formattedDate}
-                </span>
-                <span className="text-primary flex-1 text-sm font-semibold">
-                  {post.title}
-                </span>
-                {post.views > 0 && (
-                  <span className="text-muted-foreground w-20 text-right text-xs">
-                    {post.formattedViews} view{post.views > 1 ? "s" : ""}
-                  </span>
-                )}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </Animation>
-  );
+	return (
+		<Animation>
+			<div className="flex flex-col gap-6">
+				<ul className="flex flex-col">
+					{posts.map((post) => (
+						<li key={post.slug}>
+							<Link
+								href={`/blog/${post.slug}`}
+								aria-label={`Read blog post: ${post.title}`}
+								className="group hover:bg-muted/50 active:bg-muted/50 flex flex-col md:flex-row items-start md:items-center gap-1 md:gap-4 rounded-sm p-4 transition-all active:scale-95"
+							>
+								<div className="flex items-center gap-2 text-muted-foreground">
+									<span
+										className={cn(
+											"text-muted-foreground md:w-20 shrink-0 text-xs font-light",
+											{
+												"opacity-0": !post.showDate,
+											},
+										)}
+									>
+										{post.formattedDate}
+									</span>
+								</div>
+
+								<span className="text-foreground flex-1 text-base font-medium">
+									{post.title}
+								</span>
+
+								{post.views > 0 && (
+									<span className="hidden md:flex text-muted-foreground text-right text-xs font-light">
+										{post.formattedViews} view{post.views > 1 ? "s" : ""}
+									</span>
+								)}
+							</Link>
+						</li>
+					))}
+				</ul>
+			</div>
+		</Animation>
+	);
 }
