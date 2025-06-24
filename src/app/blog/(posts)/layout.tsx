e1b5844e7c676afc8046cf801b3716f124b1f98d
Alexandre Trotel <trotelalexandre@proton.me>
2025-06-24 19:53:46 +0200

    feat: add blog layout and post pages with dynamic metadata generation and initial content

---
+++ src/app/blog/(posts)/layout.tsx
@@ -0,0 +1,13 @@
+import { Animation } from "@/components/animation";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<Animation>
+			<article className="mx-auto max-w-3xl">
+				<div className="prose prose-gray dark:prose-invert max-w-none">
+					{children}
+				</div>
+			</article>
+		</Animation>
+	);
+}
