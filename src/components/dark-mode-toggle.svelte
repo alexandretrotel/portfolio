979ca21b326f861a85d661e64c1ed354f82f445e
Alexandre Trotel <trotelalexandre@proton.me>
2025-12-11 14:20:39 +0100

    fix: standardize icon sizes in dark mode toggle component

--- src/components/dark-mode-toggle.svelte
+++ src/components/dark-mode-toggle.svelte
@@ -18,10 +18,10 @@
   type="button"
 >
   <Sun
-    class="dark:-rotate-90 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:scale-0"
+    class="dark:-rotate-90 h-4 w-4 rotate-0 scale-100 transition-all dark:scale-0"
   />
   <Moon
-    class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+    class="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
   />
   <span class="sr-only">Toggle theme</span>
 </button>
