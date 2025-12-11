2f48d3ad09c6884d552133b99f9409d38e8eabf9
Alexandre Trotel <trotelalexandre@proton.me>
2025-12-11 10:22:29 +0100

    fix: prepare migration to svelte instead of React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
@@ -14,7 +14,7 @@ export default defineConfig({
   site: "https://www.alexandretrotel.org",
   output: "server",
   adapter: vercel(),
-  integrations: [react(), mdx(), sitemap()],
+  integrations: [svelte(), mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
     resolve: {
