4ea30a918d2c30ceacd06fe4aa1bb808beffdc8c
Alexandre Trotel <trotelalexandre@proton.me>
2025-12-11 14:03:38 +0100

    fix: handle default locale in getTranslations function

--- src/i18n/translations/index.ts
+++ src/i18n/translations/index.ts
@@ -1,4 +1,4 @@
-import type { Locale } from "../locales";
+import { defaultLocale, type Locale } from "../locales";
 import { en } from "./en";
 import { fr } from "./fr";
 import type { Translations } from "./types";
@@ -7,6 +7,6 @@ export const translations: Record<Locale, Translations> = {
   fr,
 };
 
-export function getTranslations(locale: Locale): Translations {
-  return translations[locale];
+export function getTranslations(locale?: Locale): Translations {
+  return translations[locale ?? defaultLocale];
 }
