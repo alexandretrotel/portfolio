d99d3154ecce59feb9d8db98bc4194aa26b8c2bf
Alexandre Trotel <trotelalexandre@proton.me>
2025-12-11 10:56:54 +0100

    feat: add QueryClientProvider Svelte component

---
+++ src/components/query-client-provider.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
+  import type { Snippet } from "svelte";
+
+  let { children }: { children: Snippet } = $props();
+
+  const queryClient = new QueryClient();
+</script>
+
+<QueryClientProvider client={queryClient} {children}/>
