7c018cd715f557fbf2de20bbaba89fd5e6554365
atrtde <trotelalexandre@proton.me>
2026-02-23 07:13:06 -0500

    fix: replace atrtde with Alexandre Trotel

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -1,5 +1,6 @@
 export const METADATA = {
   BASE_URL: "https://www.alexandretrotel.org",
-  DESCRIPTION: "atrtde is a tech entrepreneur and full-stack developer.",
-  TITLE: "atrtde",
+  DESCRIPTION:
+    "Alexandre Trotel is a tech entrepreneur and full-stack developer.",
+  TITLE: "Alexandre Trotel",
 } as const;
