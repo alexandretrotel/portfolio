1bb846f7bc7ad0d3c683d20e5bf637651508805d
alexandretrotel <trotelalexandre@proton.me>
2026-03-10 20:28:48 -0400

    fix: update homepage hero and site description

--- src/shared/config/site.ts
+++ src/shared/config/site.ts
@@ -1,6 +1,5 @@
 export const SITE_CONFIG = {
   baseUrl: "https://www.alexandretrotel.org",
-  description:
-    "Alexandre Trotel is a tech entrepreneur and full-stack developer.",
+  description: "neophilic building software, startups, and sound",
   title: "Alexandre Trotel",
 } as const;
