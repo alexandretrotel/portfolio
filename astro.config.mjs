7f776cdcf6893d890cfffef82ba84b1be19f238c
alexandretrotel <trotelalexandre@proton.me>
2026-08-25 15:05:22 +0200

    fix: work astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 // @ts-check
+import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import rehypeExternalLinks from "rehype-external-links";
@@ -7,23 +8,13 @@ export default defineConfig({
   site: "https://www.alexandretrotel.org",
   output: "static",
   trailingSlash: "never",
+  adapter: cloudflare(),
   markdown: {
     rehypePlugins: [[rehypeExternalLinks, { target: "_blank", rel: ["noreferrer"] }]],
-  },
-  build: {
-    format: "file",
   },
   integrations: [
     sitemap({
       filter: (page) => !page.endsWith(".md") && !page.endsWith("/404.html"),
-      serialize(item) {
-        const url = new URL(item.url);
-        if (url.pathname !== "/" && !url.pathname.includes(".")) {
-          url.pathname += ".html";
-          item.url = url.toString();
-        }
-        return item;
-      },
     }),
   ],
 });
